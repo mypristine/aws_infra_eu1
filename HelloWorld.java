53fdb75053d31b522eb868a83b51b7f76d73e26b
Manish Desai <52373280+mypristine@users.noreply.github.com>
2019-07-13 17:08:07 +0530

    chages made

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,11 @@
-java HelloWorld.java
-java HelloWorld
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=5;i++)	
+		{
+			System.out.println("Hello World + "+ i);
+		}
+			System.out.println("Code successfully Executed");
+	}
+
+}
